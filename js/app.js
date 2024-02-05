f46d54bc30b77c18a503d408a99e6df6e25562f9
LucasRomanias <lucasromanias@gmail.com>
2024-02-05 11:14:47 -0500

    .

--- js/app.js
+++ js/app.js
@@ -7,35 +7,24 @@ var saveJPGButton = wrapper.querySelector("[data-action=save-jpg]");
 var saveSVGButton = wrapper.querySelector("[data-action=save-svg]");
 var canvas = wrapper.querySelector("canvas");
 var signaturePad = new SignaturePad(canvas, {
-  // It's Necessary to use an opaque color when saving image as JPEG;
-  // this option can be omitted if only saving as PNG or SVG
+
   backgroundColor: 'rgb(255, 255, 255)'
 });
 
-// Adjust canvas coordinate space taking into account pixel ratio,
-// to make it look crisp on mobile devices.
-// This also causes canvas to be cleared.
+
 function resizeCanvas() {
-  // When zoomed out to less than 100%, for some very strange reason,
-  // some browsers report devicePixelRatio as less than 1
-  // and only part of the canvas is cleared then.
+
   var ratio =  Math.max(window.devicePixelRatio || 1, 1);
 
-  // This part causes the canvas to be cleared
   canvas.width = canvas.offsetWidth * ratio;
   canvas.height = canvas.offsetHeight * ratio;
   canvas.getContext("2d").scale(ratio, ratio);
 
-  // This library does not listen for canvas changes, so after the canvas is automatically
-  // cleared by the browser, SignaturePad#isEmpty might still return false, even though the
-  // canvas looks empty, because the internal data of this library wasn't cleared. To make sure
-  // that the state of this library is consistent with visual state of the canvas, you
-  // have to clear it manually.
+
   signaturePad.clear();
 }
 
-// On mobile devices it might make more sense to listen to orientation change,
-// rather than window resize events.
+
 window.onresize = resizeCanvas;
 resizeCanvas();
 
@@ -54,10 +43,8 @@ function download(dataURL, filename) {
   window.URL.revokeObjectURL(url);
 }
 
-// One could simply use Canvas#toBlob method instead, but it's just to show
-// that it can be done using result of SignaturePad#toDataURL.
+
 function dataURLToBlob(dataURL) {
-  // Code taken from https://github.com/ebidel/filer.js
   var parts = dataURL.split(';base64,');
   var contentType = parts[0].split(":")[1];
   var raw = window.atob(parts[1]);
@@ -79,7 +66,7 @@ undoButton.addEventListener("click", function (event) {
   var data = signaturePad.toData();
 
   if (data) {
-    data.pop(); // remove the last dot or line
+    data.pop(); 
     signaturePad.fromData(data);
   }
 });
